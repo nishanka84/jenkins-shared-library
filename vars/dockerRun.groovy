daa084152b2b15174ad917d6d01ced2ced2f4a53
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 19:42:05 -0400

    Update dockerRun.groovy

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -1,11 +1,7 @@
-def call(){
+def run(){
 		         try {
-			  
-		            sh "docker stop modest_thompson "
-                            sh "docker run -d -p 80:8080 nishank/helloworld"
-		         } catch (err) {
-               
-                 echo "$err"
-				
+			 
+                            sh "docker run -d -p 80:8080 -name sharedlib nishank/helloworld"
+		         } catch (err) {	
 		         }
 }
