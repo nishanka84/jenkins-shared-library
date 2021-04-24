cbed30897937337f723eecce410ab425f0fbd9fb
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 12:12:15 -0400

    Update passout.groovy

--- vars/passout.groovy
+++ vars/passout.groovy
@@ -1,10 +1,8 @@
 
 def call(String name = "master"){
     stage ('test') {
-        steps {
             script {
                 git branch: ${name}, url: "https://github.com/nishanka84/HelloTesting-World"
             }
-        }
     }
 }
