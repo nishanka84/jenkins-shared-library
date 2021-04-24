f5939d688ca808dc34b43301798bc771e8ab3474
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 10:11:42 -0400

    Create sources.groovy

---
+++ vars/sources.groovy
@@ -0,0 +1,10 @@
+import org.example.vars
+def call(Map config) {
+    if (config.type == "slack") {
+        echo vars.SLACK_MESSAGE
+        echo config.message
+    } else {
+        echo vars.EMAIL_MESSAGE
+        echo config.message
+    }
+}
