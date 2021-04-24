a46499859e033611d8884d19d06a961ff4423202
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 10:43:26 -0400

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,11 +1,8 @@
 def call(Map config){
   
-  		   if (params.Git == true) {
-             
-                       git branch: ${config.master}, url: ${config.url}
-                         
-		                   
-		     } else {
-			                 echo " Git or Branch details not specified"
-	       }
+        if (params.Git == true) {
+              git branch: ${config.master}, url: ${config.url}                   
+	} else {
+	      echo " Git or Branch details not specified"
+        }
 }
