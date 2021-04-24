14117b4e12b39b72d00026717d3383fba44f606e
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 10:48:30 -0400

    Delete checkout.groovy

--- vars/checkout.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map config){
-  
-        if (params.Git == true) {
-              git branch: ${config.master}, url: ${config.url}                   
-	} else {
-	      echo " Git or Branch details not specified"
-        }
-}
