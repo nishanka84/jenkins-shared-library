a2ee4ebd3d65acc63af5f43a069294bd29e0629e
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 18:43:34 -0400

    Create dockerBuild.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,10 @@
+def execute() {		   
+	              catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+		                     sh "docker build -t helloworld:latest ." 
+		                     sh "docker tag helloworld nishank/helloworld:latest"
+
+	              }              
+           }
+       
+        }
+}
