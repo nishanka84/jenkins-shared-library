ab3391f312684935b5c10fbdaa6cf81da60797d9
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 13:03:44 -0400

    Update parallel.groovy

--- vars/parallel.groovy
+++ vars/parallel.groovy
@@ -1,15 +1,15 @@
 def call(Map config) {
 		      parallel(
 			      ubuntu: {
-				      echo "Test config.ubuntu"
+				      echo "Test config.ubun"
 				      sleep 10
 			      },
 			      windows: {
-				      echo "Test config.windows"
+				      echo "Test config.win"
 				      sleep 10
 			      },
 			      unix: {
-				      echo "Test config.unix"
+				      echo "Test config.uni"
 				      sleep 10
 			      }
 		        )	      
