5304e8bead1f5febb545c645d634a0023cc4a1ef
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 19:51:19 -0400

    Update dockerRun.groovy

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -1,6 +1,6 @@
 def kube(){
 		         try {
-			   
+			    sh "docker stop --name sharedlib"
                             sh "docker run -d -p 80:8080 --name sharedlib nishank/helloworld"
 		         } catch (err) {	
 		         }
