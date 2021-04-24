4acabf5b683607c7daede11759264b7c6325c8a6
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 10:19:56 -0400

    Update sources.groovy

--- vars/sources.groovy
+++ vars/sources.groovy
@@ -1,10 +1,10 @@
 import org.example.Constants
 def call(Map config) {
     if (config.type == "slack") {
-        echo vars.SLACK_MESSAGE
+        echo Constants.SLACK_MESSAGE
         echo config.message
     } else {
-        echo vars.EMAIL_MESSAGE
+        echo Constants.EMAIL_MESSAGE
         echo config.message
     }
 }
