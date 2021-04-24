a858e9669f0b72c0a3c78754820bd322ed9d1054
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 11:50:54 -0400

    Update passout.groovy

--- vars/passout.groovy
+++ vars/passout.groovy
@@ -1,8 +1,5 @@
 def call(Map config){
 
-  if (${config.choice} == true) {
               git branch: ${config.branch}, url: ${config.url}                   
-	} else {
-	      echo " Git or Branch details not specified"
-        }
+
 }
