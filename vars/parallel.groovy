c7a397972825a82ac89bf190036a98de053ba411
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 10:33:02 -0400

    Create parallel.groovy

---
+++ vars/parallel.groovy
@@ -0,0 +1,17 @@
+def call(Map config){
+  		      
+            parallel(
+			      ubuntu: {
+              echo "Test ${config.ubuntu}"
+				      sleep 10
+			      },
+			      windows: {
+              echo "Test ${config.windows}"
+				      sleep 10
+			      },
+			      unix: {
+              echo "Test ${config.unix}"
+				      sleep 10
+			      }
+            )
+}
