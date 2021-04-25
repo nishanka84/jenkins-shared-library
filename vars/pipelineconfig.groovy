6a12b985720222e5a2906588a2c05bf2a92caa1f
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 20:21:15 -0400

    Create pipelineconfig.groovy

---
+++ vars/pipelineconfig.groovy
@@ -0,0 +1,17 @@
+def call(String filename) {
+try{
+    dir ("${WORKSPACE}") {
+      def exists = fileExists filename
+      if (exists) {
+        println "Config file [${workspace}/" + filename + "] found"
+        def file = readYaml file: filename  
+        return file
+      } else {
+          println "Config file [${workspace}/" + filename + "] not found"
+          throw new Exception("Config file [${workspace}/" + filename + "] not found")
+      }
+    }
+  } catch (err) {
+      throw err
+  }
+}
