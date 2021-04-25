dabe67f11ea750db4174dcb84950f7d62e3de21c
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-24 20:38:19 -0400

    Update pipelineconfig.groovy

--- vars/pipelineconfig.groovy
+++ vars/pipelineconfig.groovy
@@ -4,7 +4,7 @@ try{
       def exists = fileExists filename
       if (exists) {
         println "Config file [${workspace}/" + filename + "] found"
-        def file = readFile file: filename  
+        def file = readFile "${workspace}/filename" 
         return file
       } else {
           println "Config file [${workspace}/" + filename + "] not found"
