07aca972b0d17f00fa382ff7f6bddcc35348f22f
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-26 13:21:31 -0400

    Update pipelineCfg.groovy

--- vars/pipelineCfg.groovy
+++ vars/pipelineCfg.groovy
@@ -1,20 +1,3 @@
-/*def call(String filename) {
-try{
-    dir ("${WORKSPACE}") {
-      def exists = fileExists filename
-      if (exists) {
-        println "Config file [${workspace}/" + filename + "] found"
-        def file = readFile(file: 'config.yaml')
-        return file
-      } else {
-          println "Config file [${workspace}/" + filename + "] not found"
-          throw new Exception("Config file [${workspace}/" + filename + "] not found")
-      }
-    }
-  } catch (err) {
-      throw err
-  }
-}*/
 
 def call() {
   def pipelineCfg = readFile(file: "${WORKSPACE}/config.yaml")
