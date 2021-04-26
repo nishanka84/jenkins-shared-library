277d87da702ac2eb963d159e58b0550b6fb32a09
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-04-26 07:18:56 -0400

    Update testingSomething.groovy

--- vars/testingSomething.groovy
+++ vars/testingSomething.groovy
@@ -1,6 +1,7 @@
 def call(Map config) {
   if (config.goat == 'true') {
              echo "GOAT is true"
+             echo "config.cat is a nice pet"
   } else {
              echo "GOAT is false"
   }
