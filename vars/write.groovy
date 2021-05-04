f67b19701227a7e67fd14f1b31b49e27035b78f5
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-05-04 15:58:16 -0400

    Update write.groovy

--- vars/write.groovy
+++ vars/write.groovy
@@ -1,7 +1,7 @@
 def call(Map config){
   dir ("${env.WORKSPACE}") {
-  writeFile file: "${env.WORKSPACE}/testing.txt", text: "config.texting."
-//  sh 'cat testing.txt'
+  writeFile file: "testing.txt", text: "config.texting."
+  sh 'cat testing.txt'
   }  
   
 }
