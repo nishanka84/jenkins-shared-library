5eb4cb4871c126a66244609eefc402d05b4ebc4e
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-05-04 16:14:59 -0400

    Update write.groovy

--- vars/write.groovy
+++ vars/write.groovy
@@ -1,7 +1,15 @@
 def call(Map config){
+  checkoutScm("${config.gitUrl}", "refs/heads/${config.branch}")
   dir ("${env.WORKSPACE}") {
     writeFile file: "testing.txt", text: "${config.texting}"
   sh 'cat testing.txt'
+               sshagent(['git-id']) {
+                sh("git add ${testing.txt}")
+                sh("git commit -m 'Updated file'")
+                sh("git pull origin refs/heads/${config.branch}") 
+                sh("git push origin HEAD:refs/heads/${config.branch}") 
+            }
+
   }  
   
 }
