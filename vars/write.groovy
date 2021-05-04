342d423615e976666b339327d54be27f24ca7926
nishanka84 <35771287+nishanka84@users.noreply.github.com>
2021-05-04 16:52:15 -0400

    Update write.groovy

--- vars/write.groovy
+++ vars/write.groovy
@@ -1,6 +1,7 @@
 def call(Map config){
  // git url:"${config.gitUrl}", branch: "refs/heads/${config.type}"
   dir ("${env.WORKSPACE}") {
+    checkoutScm("${config.gitUrl}", "refs/heads/${config.branch}")
     writeFile file: "testing.txt", text: "${config.texting}"
   sh 'cat testing.txt'
                sshagent(['git-id']) {
